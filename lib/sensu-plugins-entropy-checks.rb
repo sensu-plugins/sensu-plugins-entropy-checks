75d5b5773a9ace84bb27027f384500571091ba23
Matt Jones <mattjones@yieldbot.com>
2015-06-02 23:19:28 -0400

    deploy

--- lib/sensu-plugins-entropy-checks.rb
+++ lib/sensu-plugins-entropy-checks.rb
@@ -1,14 +1,1 @@
 require 'sensu-plugins-entropy-checks/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsEntropyChecks
-  class << self
-  end
-
-  class << self
-  end
-end
